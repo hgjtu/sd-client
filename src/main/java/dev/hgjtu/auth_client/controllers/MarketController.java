9ae90e2be81c01f66db461f7075c27e1d43fd3df
hgjtu <vlrtmsq@gmail.com>
2025-10-19 18:17:27 +0300

    схуяли все сломалось то блять

--- src/main/java/dev/hgjtu/auth_client/controllers/MarketController.java
+++ src/main/java/dev/hgjtu/auth_client/controllers/MarketController.java
@@ -1,5 +1,6 @@
 package dev.hgjtu.auth_client.controllers;
 
+import dev.hgjtu.auth_client.services.MarketService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,30 +8,49 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/market")
 public class MarketController {
+    private final MarketService marketService;
+
     @GetMapping
-    public String market() {
-        return "market/market-dashboard";
+    public Mono<String> market(Model model) {
+        return marketService.getAllCategories()
+                .collectList()
+                .map(categories -> {
+                    model.addAttribute("categories", categories);
+                    return "market/market-dashboard";
+                })
+                .onErrorResume(Exception.class, e -> {
+                    model.addAttribute("error", "Ошибка при вызове API: " + e.getMessage());
+                    return Mono.just("market/market-dashboard");
+                });
     }
 
     @GetMapping("/category/{slug}")
-    public String category(@PathVariable String slug,
-                           @RequestParam(defaultValue = "buy") String mode,
-                           Model model) {
-//        // Получаем человекочитаемое имя категории (можно сделать маппинг в сервисе или enum)
-//        String categoryName = itemService.getCategoryDisplayName(slug);
-//
-//        // Получаем список товаров для выбранной категории и режима
-//        List<ItemMinResponse> items = itemService.getItemsByCategoryAndMode(slug, mode);
-
-        // Передаём данные в шаблон
-        model.addAttribute("categoryName", slug);
-//        model.addAttribute("items", items);
-        model.addAttribute("mode", mode);
-        return "market/category";
+    public Mono<String> category(@PathVariable String slug,
+                                 @RequestParam(defaultValue = "buy") String mode,
+                                 Model model) {
+        return marketService.getCategoryByName(slug)
+                .flatMap(category ->
+                        marketService.getItemsByCategory(slug)
+                                .collectList()
+                                .map(items -> {
+                                    model.addAttribute("categoryName", category.getNameRu());
+                                    model.addAttribute("categoryDescription", category.getDescription());
+                                    model.addAttribute("mode", mode);
+                                    model.addAttribute("items", items);
+                                    return "market/category";
+                                })
+                )
+                .onErrorResume(Exception.class, e -> {
+                    model.addAttribute("error", "Ошибка при вызове API: " + e.getMessage());
+                    return Mono.just("market/category");
+                });
     }
+
 }
---
+++ src/main/java/dev/hgjtu/auth_client/services/MarketService.java
@@ -0,0 +1,51 @@
+package dev.hgjtu.auth_client.services;
+
+import dev.hgjtu.auth_client.dto.CategoryResponse;
+import dev.hgjtu.auth_client.dto.ItemMinResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class MarketService {
+    private final WebClient webClient;
+
+
+    @Value("${MARKET_RESOURCE_SERVER_URL}")
+    private String marketResourceServerUrl;
+
+    public Flux<CategoryResponse> getAllCategories() {
+        return webClient.get()
+                .uri(marketResourceServerUrl + "/api/categories")
+                .retrieve()
+                .bodyToFlux(CategoryResponse.class);
+    }
+
+//    public Mono<CategoryResponse> getCategoryById(Integer id) {
+//        return webClient.get()
+//                .uri(marketResourceServerUrl + "/api/categories/{id}", id)
+//                .retrieve()
+//                .bodyToMono(CategoryResponse.class);
+//    }
+
+    public Mono<CategoryResponse> getCategoryByName(String name) {
+        return webClient.get()
+                .uri(marketResourceServerUrl + "/api/categories/{name}", name)
+                .retrieve()
+                .bodyToMono(CategoryResponse.class);
+    }
+
+    public Flux<ItemMinResponse> getItemsByCategory(String category) {
+        Flux<ItemMinResponse> qw = webClient.get()
+                .uri(marketResourceServerUrl + "/api/items/all-by-category/{category}", category)
+                .retrieve()
+                .bodyToFlux(ItemMinResponse.class);
+
+        return qw;
+    }
+
+}
