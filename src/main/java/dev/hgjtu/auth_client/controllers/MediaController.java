97d1037aab91cf220e404d9f10edee983e3fd784
hgjtu <vlrtmsq@gmail.com>
2026-02-19 22:02:22 +0300

    почти получилось

--- src/main/java/dev/hgjtu/auth_client/controllers/MediaController.java
+++ src/main/java/dev/hgjtu/auth_client/controllers/MediaController.java
@@ -9,23 +9,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class MediaController {
     private final MediaService mediaService;
 
-    @PostMapping("/media/{resource-name}/upload-url")
+    @PostMapping("/media/{resourceName}/upload-url")
     public Mono<MediaUploadResponse> uploadUrl(@RequestBody UploadUrlRequest uploadUrlRequest,
-                                               @PathVariable("resource-name") AvailableResources resourceName) {
+                                               @PathVariable AvailableResources resourceName) {
         return mediaService.getUploadUrl(resourceName, uploadUrlRequest);
     }
 
-    @PostMapping("/media/{resource-name}/complete/{mediaId}")
-    public Mono<MediaUploadResponse> completeMedia(@PathVariable("resource-name") AvailableResources resourceName,
+    @PostMapping("/media/{resourceName}/complete/{mediaId}")
+    public Mono<MediaUploadResponse> completeMedia(@PathVariable AvailableResources resourceName,
                                                    @PathVariable UUID mediaId) {
         return mediaService.completeMedia(resourceName, mediaId);
     }
